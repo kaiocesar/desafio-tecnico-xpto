cf7e973a34dadb41a35c0a388ac0042a6d482c46
Kaio Cesar <tecnico.kaio@gmail.com>
2026-03-30 23:59:44 -0300

    feat: add catalog_route basic endpoint and  basic integration test

--- app/modules/catalog/catalog_routes.py
+++ app/modules/catalog/catalog_routes.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
 
+from app.modules.catalog.catalog_depency_injection import get_repository
+from app.modules.catalog.repositories.award_repository_sqlalchemy import AwardRepositorySQLAlchemy
+
 router = APIRouter(prefix="/catalog", tags=["catalog"])
 
 @router.get("/awards")
-def list_awards():
-    return {"min": [], "max": []}
+def list_awards(repo = Depends(get_repository(AwardRepositorySQLAlchemy))):
+    return repo.find_all()
