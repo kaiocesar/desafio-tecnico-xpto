c130c138d8865d90a1d3da1a18faf10607f4fd82
Kaio Cesar <tecnico.kaio@gmail.com>
2026-03-30 23:59:54 -0300

    feat: add catalog_route basic endpoint and  basic integration test

--- tests/integration/modules/catalog/test_catalog_routes.py
+++ tests/integration/modules/catalog/test_catalog_routes.py
@@ -3,4 +3,4 @@
 def test_get_awards(client):
     response = client.get("/api/v1/catalog/awards")
     assert response.status_code == 200
-    assert response.json() == {"min": [], "max": []}
+    assert response.json() == [] # {"min": [], "max": []}
